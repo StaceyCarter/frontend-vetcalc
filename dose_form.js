505e7a8d991b3302723a32e86357b49aa03ec309
StaceyCarter <stak1903@gmail.com>
2019-05-27 14:34:11 -0700

    adding structure to react form

---
+++ dose_form.js
@@ -0,0 +1,103 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import anime from 'animejs/lib/anime.es.js'
+
+
+function Weight(props) {
+  return (
+  <div>
+    <label>
+      Weight: <input type="text" name="weight" />
+    </label>
+    <div className="btn-group btn-group-toggle" data-toggle="buttons">
+      <label className="btn btn-secondary active">
+        <input type="radio" name="kg" id="option1" autoComplete="off" /> kg
+      </label>
+      <label className="btn btn-secondary">
+        <input type="radio" name="options" id="option2" autoComplete="off" /> lbs
+      </label>
+    </div>
+  </div>
+  )
+}
+
+function LiqOrTabs(props) {
+  return (
+    <div>
+      <label>
+        <input type="radio" name="liqTab" value="liq" /> Liquid 
+      </label> 
+      <label>
+        <input type="radio" name="liqTab" value="tab" /> Tablet 
+      </label>
+    </div>
+  )
+}
+
+function Route(props) {
+  return (
+    <div>
+      <select>
+        <option value="PO">PO</option>
+        <option value="OU">OU</option>
+        <option value="OD">OD</option>
+        <option value="OS">OS</option>
+        <option value="SQ">SQ</option>
+        <option value="IM">IM</option>
+      </select>
+    </div>
+  )
+}
+
+function Concentration(props){
+  return (
+    <div>
+      <label>What is the concentration? 
+        <input type="number" name="concentration" step="0.01" />
+      </label>
+    </div>
+  )
+}
+
+function Divisions(props){
+  return (
+    <div>
+      <label>
+        How many pieces can your tablets be divided into?
+        <input type="number" name="divide" /> pieces
+      </label>
+    </div>
+  )
+}
+
+function DosePicker(props){
+  return (
+    <div>
+      <label>
+        What dose do you want to use? (in mg/kg) <input type="number" name="dose" step="0.01" required />
+      </label>
+    </div> 
+  )
+}
+
+
+class Form extends React.Component{
+  render(){
+    return (
+    <div>
+    <h1>Hello world</h1>
+    <Weight />
+    <LiqOrTabs />
+    <Route />
+    <Concentration />
+    <Divisions />
+    <DosePicker />
+    </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <Form />,
+  document.getElementById('root')
+)
---
+++ slider_test.js
@@ -0,0 +1,80 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import anime from 'animejs/lib/anime.es.js'
+
+
+
+
+
+class Slider extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {value: ''}
+
+    this.handleChange = this.handleChange.bind(this)
+
+  }
+
+  handleChange(event){
+    this.setState({value : event.target.value})
+    anime({
+      targets: '.box',
+      height: (this.state.value),
+      easing: 'linear',
+      direction: 'normal',
+    });
+  }
+
+  render() {
+    return(
+    <div>
+      <ValueInfo value={this.state.value} />
+      <input type="range" min="1" max="100" list="tickmarks" value={this.state.value} onChange={this.handleChange} id="slider"></input>
+      <datalist id="tickmarks">
+        <option value="1" label="0%" />
+        <option value="50" label="50%" />
+        <option value="100" label="100%" />
+      </datalist>
+    </div>
+    )
+  }
+}
+
+class ValueInfo extends React.Component{
+  render(){
+    return(
+      <div>
+        <h1>{this.props.value}</h1>
+      </div>
+    )
+  }
+}
+
+class Box extends React.Component{
+  render() {
+    return (
+    <div className='box-container'>
+      <div className='box'></div>
+    </div>
+    )
+  }
+}
+
+class App extends React.Component{
+  render(){
+    return (
+    <div>
+    <h1>Hello world</h1>
+    <Slider />
+    <Box />
+    </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
+
+
