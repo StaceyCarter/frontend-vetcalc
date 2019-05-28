345d16a297b9ce79f8c6f2231cbe1e1fdaf8236f
StaceyCarter <stak1903@gmail.com>
2019-05-27 17:36:53 -0700

    able to select kg or lbs, will change state accordingly

--- weight.js
+++ weight.js
@@ -1,31 +1,87 @@
-import React from "react"
+import React from "react";
 
-function KgOrLbs(props){
-  return(
-    <div className="btn-group btn-group-toggle" data-toggle="buttons">
-      <label className="btn btn-secondary active">
-        <input type="radio" name="kg" id="option1" autoComplete="off" /> kg
-      </label>
-      <label className="btn btn-secondary">
-        <input type="radio" name="options" id="option2" autoComplete="off" /> lbs
-      </label>
-    </div>
-  )
+class KgOrLbs extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedOption: "kg"
+    };
+
+    this.handleOptionChange = this.handleOptionChange.bind(this);
+  }
+
+  handleOptionChange(evt) {
+    this.setState({
+      selectedOption: evt.target.value
+    }, 
+    () => this.props.changeUnit(this.state.selectedOption));
+  }
+
+  render() {
+    return (
+      <div className="weight-selector">
+        <label>
+          <input
+            type="radio"
+            value="kg"
+            checked={this.state.selectedOption==="kg"}
+            onChange={this.handleOptionChange}
+          />
+          kg
+        </label>
+        <label>
+          <input
+            type="radio"
+            value="lbs"
+            checked={this.state.selectedOption==="lbs"}
+            onChange={this.handleOptionChange}
+          />
+          lbs
+        </label>
+      </div>
+    );
+  }
 }
 
 // Renders a weight input form and sets weight state in Form.
-export default function Weight(props) {
-  const setWeightFromEvent = (e) => {
-    props.setWeight(parseFloat(e.target.value))
+export default class Weight extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      units: "kg"
+    };
+
+    this.setWeightFromEvent = this.setWeightFromEvent.bind(this);
+    this.changeUnit = this.changeUnit.bind(this);
   }
 
-  return (
-  <div>
-    <label>
-      Weight: <input type="text" name="weight" value={props.weight} onChange={setWeightFromEvent}/>
-    </label>
-    <KgOrLbs />
-  </div>
-  )
-}
+  setWeightFromEvent(e) {
+    this.props.setWeight(parseFloat(e.target.value));
+  }
 
+  changeUnit(newUnit) {
+    this.setState({
+      units: newUnit
+    }, () => console.log("new unit for main weight component: ", this.state.units));
+    
+  }
+
+  render() {
+    return (
+      <div>
+        <label>
+          Weight:
+          <input
+            type="text"
+            name="weight"
+            value={this.props.weight}
+            onChange={this.setWeightFromEvent}
+          />
+        </label>
+        <KgOrLbs changeUnit={this.changeUnit}/>
+      </div>
+    );
+  }
+}
